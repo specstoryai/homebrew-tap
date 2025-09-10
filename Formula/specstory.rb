ca791921c87c6b7acf1c12a1186fba413f2715d0
Sean Johnson <sean.johnson@stoatlabs.com>
2025-09-10 18:28:52 -0400

    Release 0.9.1

--- Formula/specstory.rb
+++ Formula/specstory.rb
@@ -1,25 +1,25 @@
 class Specstory < Formula
   desc "A claude code wrapper that saves your conversation history to markdown"
   homepage "https://github.com/specstoryai/getspecstory"
-  version "0.9.0"
+  version "0.9.1"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/specstoryai/getspecstory/releases/download/v#{version}/SpecStoryCLI_Darwin_arm64.tar.gz"
-      sha256 "27a73c30cb9d5c6fae8d42ee06d2f4c6effc90d501d4639787edafaf30544a59"
+      sha256 "71758f0c7dc2d34fe132ca1424e14411c760de61ead9f56af9a9582120bc5f6d"
     else
       url "https://github.com/specstoryai/getspecstory/releases/download/v#{version}/SpecStoryCLI_Darwin_x86_64.tar.gz"
-      sha256 "fd0fb029b1e980daa1d8b6863d0d3d791808debf7ead93b5bf4a0499528ca347"
+      sha256 "727dec280f082fa3815e85d002e7364cc2e740894d6185e97c0de45aa12c824e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/specstoryai/getspecstory/releases/download/v#{version}/SpecStoryCLI_Linux_arm64.tar.gz"
-      sha256 "052ed4d36df10125392eb550ce2404d67507514af11cc0d880c85545bdb855e5"
+      sha256 "f5be0cc56f7b2f58f498cc0e3ff527f23b09d003aed02a69ef86e11b96aa3d83"
     else
       url "https://github.com/specstoryai/getspecstory/releases/download/v#{version}/SpecStoryCLI_Linux_x86_64.tar.gz"
-      sha256 "de93e2c15625506fcbf2305378de92134e4aed022646a58a137244ada5ab63db"
+      sha256 "f33d8042553c4bfcb5adee5c46ce3842fddb5ea78bfd8b817355126335f1062f"
     end
   end
 
